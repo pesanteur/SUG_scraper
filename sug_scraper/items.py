f53a1c760875e159b19f3e64b2e2b5b0b335b0a7
Mathue Duhaney <pesanteur123@gmail.com>
2018-01-26 05:25:25 -0800

    Added more scraping logic. Also running spider now works and generates a basic csv. Need to correct for loop and improve scraping logic.

--- sug_scraper/items.py
+++ sug_scraper/items.py
@@ -5,10 +5,15 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item
+from scrapy.item import Field
 
 
-class SugScraperItem(scrapy.Item):
+class SugScraperItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # Date = Field()
+    #Location = Field()
+    Time = Field()
+    Status = Field()
+    DES_Group = Field()
--- sug_scraper/spiders/spider.py
+++ sug_scraper/spiders/spider.py
@@ -1,27 +1,33 @@
 from scrapy import Spider
-
+from scrapy import log
+from sug_scraper.items import SugScraperItem
 
 class SUGSpider(Spider):
-
     name = "SUG"
     allowed_domains = ["signupgenius.com"]
+    start_urls = ('http://www.signupgenius.com/go/10c084baaa82da2fe3-term227',)
 
     def parse(self, response):
         #TODO: tidy this up and make it more generalized
         log.msg('parse(%s)' % response.url, level = log.DEBUG)
         rows = response.xpath('//table[@class="SUGtableouter"]/tr')
+        """
         for row in rows:
-            item = ScheduleItem()
             item['Date'] = response.xpath('/tr[2]/td/span/text()').extract()[0] #first row of tables
             item['Location'] = response.xpath('/tr[2]/td/span/text()').extract()[1]
             item['Time'] = response.xpath('/tr[2]/td/span/text()').extract()[2]
-            item['DES Group'] = response.xpath('/tr[2]/td/table/tr/td/span/text()').extract() #sub table in first row
+            item['DES_Group'] = response.xpath('/tr[2]/td/table/tr/td/span/text()').extract() #sub table in first row
             item['Status'] = response.xpath('/tr[2]/td/table/tr/td[3]/div/span/span/text()').extract() #sign up/already filled
+        """
         for row in rows:
-            item['Date'] = row.xpath('td/span/text()').extract()[0]
-            item['Location'] = row.xpath('td/span/text()').extract()[1]
-            #item['Time'] = row.xpath('td/span/text()').extract()[2]
-            item['DES Group'] = row.xpath('td/table/tr/td/span/text()').extract()[0]
+            #TODO: Don't need a for loop creates errors fix this
+            item = SugScraperItem()
+            #item['Date'] = row.xpath('td/span/text()').extract()[0]
+            #item['Location'] = row.xpath('td/span/text()').extract()[1]
+            item['Time'] = row.xpath('td[1]/span/text()').extract()
+            item['DES_Group'] = row.xpath('td/table/tr/td/span[@class="SUGbigbold"]/text()').extract()
+            item['Topic'] = row.xpath('td/table/tr/td/span[@class="SUGcomment"]/text()').extract()
             item['Status'] = row.xpath('td/table/tr/td/div/span/span/text()').extract()
             #TODO: check if status is empty if it is check for "Already filled"
-            item['Status'] = row.xpath('td/table/tr/td/div/span/text()').extract()
+            #item['Status'] = row.xpath('td/table/tr/td/div/span/text()').extract()
+            yield item
